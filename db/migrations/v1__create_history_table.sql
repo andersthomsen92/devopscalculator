2a4142d3386de1be90e12705552f32a1ed7aa3ff
andersthomsen92 <doomknight92@gmail.com>
2025-03-17 11:38:59 +0100

    sql tests

--- db/migrations/v1__create_history_table.sql
+++ db/migrations/v1__create_history_table.sql
@@ -1,5 +1,9 @@
-Create table history (
-    id int primary key auto_increment,
-    text varchar(255),
-    created_at timestamp default current_timestamp
+-- Create the schema if it doesn't exist
+CREATE SCHEMA IF NOT EXISTS mydatabase;
+
+-- Create the history table within that schema
+CREATE TABLE IF NOT EXISTS mydatabase.history (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        text VARCHAR(255),
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
